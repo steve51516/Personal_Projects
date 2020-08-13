d5c2a674d84e0cc2b159ddbaf5f5e3d032670d85
Steven Fairchild <steve51516@gmail.com>
2020-08-13 11:55:50 -0400

    added FindMissingNum program

---
+++ FindMissingNum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TestCodeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random random = new Random();
+            int[] vs = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                vs[i] = i;
+            }
+            vs[50] = 0;
+
+            Find find = new Find();
+
+            int missingInt = find.FindMissing(vs);
+            System.Console.WriteLine(missingInt);
+        }
+
+    }
+    public class Find
+    {
+        public int FindMissing(int[] vs)
+        {
+            for (int i = 0; i < vs.Length - 1; i++)
+            {
+                 if (vs[i] != i)
+                 {
+                    System.Console.WriteLine($"{i} is the missing number");
+                    return i;
+                 }
+            }
+            System.Console.WriteLine("No missing number found.");
+            return 0;
+
+        }
+    }
+}
