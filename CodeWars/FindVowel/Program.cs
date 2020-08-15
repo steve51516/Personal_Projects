2be6b95367aa50cbc69e53b1d9cbc01a810ecaa2
Steven Fairchild <steve51516@gmail.com>
2020-08-15 15:44:16 -0400

    added FindVowel program

---
+++ CodeWars/FindVowel/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FindVowel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please enter a string to test for vowels: ");
+            int vowelCount = Kata.GetVowelCount(Console.ReadLine());
+            System.Console.WriteLine($"The vowel count is {vowelCount}");
+        }
+    }
+public static class Kata
+{
+    public static int GetVowelCount(string str)
+    { // a, e, i, o, u
+        int vowelCount = 0;
+
+        char[] arr = str.ToCharArray();
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            switch (arr[i])
+            {
+                case ('a'):
+                vowelCount++;
+                break;
+                case ('e'):
+                vowelCount++;
+                break;
+                case ('i'):
+                vowelCount++;
+                break;
+                case ('o'):
+                vowelCount++;
+                break;
+                case ('u'):
+                vowelCount++;
+                break;
+                default:
+                break;
+            }
+        }
+
+        return vowelCount;
+    }
+}
+
+}
