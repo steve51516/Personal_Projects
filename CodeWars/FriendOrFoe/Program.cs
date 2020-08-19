9e6a4437a0b4813534b7bb24f5072fec3c58dd5c
Steven Fairchild <steve51516@gmail.com>
2020-08-18 22:22:03 -0400

    added FriendOrFoe

---
+++ CodeWars/FriendOrFoe/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendOrFoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Ryan", "Kieran", "Mark" };
+            string[] friends = (string[])Kata.FriendOrFoe(names);
+            foreach (string name in friends)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+    class Kata
+    {
+        public static IEnumerable<string> FriendOrFoe(string[] names)
+        {
+            int friendCount = 0;
+            for (int i = 0; i <= names.Length - 1; i++)
+            {
+                if (names[i].Length == 4)
+                    friendCount++;
+            }
+            int j = 0;
+            string[] friends = new string[friendCount];
+            for (int i = 0; i <= names.Length - 1; i++)
+            {
+                if (names[i].Length == 4)
+                {
+                    friends[j] = names[i];
+                    j++;
+                }
+            }
+            return friends;
+        }
+    }
+}
--- CodeWars/RomanCypher/CodeWarsUpload.cs
+++ CodeWars/RomanCypher/CodeWarsUpload.cs
@@ -15,7 +15,7 @@ class Kata
                     else
                         vs[i] += (char)13;
                 }
-                else if ((int)vs[i] >= 97 & (int)vs[i] <= 122)
+                else if (vs[i] >= 97 & vs[i] <= 122)
                 {
                     if (vs[i] >= 110)
                     {
