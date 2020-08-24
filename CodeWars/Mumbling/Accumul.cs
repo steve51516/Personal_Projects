3790878959f99a96bda4a5663fcc4b10d3c2dada
Steven Fairchild <steve51516@gmail.com>
2020-08-24 12:01:44 -0400

    added Mumbling program

---
+++ CodeWars/Mumbling/Accumul.cs
@@ -0,0 +1,28 @@
+﻿namespace Mumbling
+{
+    public class Accumul
+    {
+        public static string Accum(string s)
+        {
+            string final = "";
+            int k = 1, j = k;
+
+            for (int i = 0; i <= s.Length - 1; i++)
+            {
+                while (j > 0)
+                {
+                    if (j == k)
+                        final += s.ToUpper()[i];
+                    else
+                        final += s.ToLower()[i];
+                    j--;
+                }
+                    final += (char)45;
+                k++;
+                j = k;
+            }
+            final = final.Remove(final.Length - 1);
+            return final;
+        }
+    }
+}
---
+++ CodeWars/Mumbling/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mumbling
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //string mumble = Accumul.Accum("ZpglnRxqenU");
+        }
+    }
+}
