be7370e50c274bc1394aff0ac6ebe873dd681ec4
Steven Fairchild <steve51516@gmail.com>
2020-09-01 21:08:16 -0400

    corrected code

--- CodeWars/StadiumWave/Program.cs
+++ CodeWars/StadiumWave/Program.cs
@@ -10,7 +10,7 @@ namespace StadiumWave
     {
         static void Main(string[] args)
         {
-            List<string> waveList = Stadium.Wave("codewars");
+            List<string> waveList = Stadium.Wave("gap");
 
             foreach (var wave in waveList)
             {
@@ -23,23 +23,25 @@ namespace StadiumWave
         public static List<string> Wave(string str)
         {
             List<char> wave = new List<char>();
-            for (int i = 0, j = 0; j < Math.Pow(str.Length, 2) - 7;j++)
+            char[] strChar = str.ToCharArray();
+            for (int i = 0, j = 0; j <= Math.Pow(str.Length, 2); j++)
             {
-                if (j % 8 == 0)
+                if (j == Math.Pow(str.Length, 2) || j % 8 == 0)
                 {
-                    wave.Add(char.ToUpper(str[i]));
+                    strChar[i] = char.ToUpper(strChar[i]);
+                    wave.AddRange(strChar);
+                    wave.Add(',');
+                    strChar[i] = char.ToLower(strChar[i]);
                     i++;
                 }
-                else if (str[i] != str[i - 1])
-                    wave.Add(str[i]);
             }
-            List<string> strList = new List<string>();
             string tempStr = "";
             for (int i = 0; i < wave.Count; i++)
             {
                 tempStr += wave[i];
             }
-            strList.Add(tempStr);
+            var vs = tempStr.Split(',');
+            List<string> strList = vs.ToList();
             return strList;
         }
     }
