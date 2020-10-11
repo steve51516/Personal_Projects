4015e42169146b42d5a54d55a07a3303ab2100bf
Steven Fairchild <steve51516@gmail.com>
2020-10-10 21:09:19 -0400

    edited sed options

--- CodeWars/replace_vowels.sh
+++ CodeWars/replace_vowels.sh
@@ -2,7 +2,7 @@
 # Script that replaces all vowels from input string
 if [ -n "$1" ];
 then
-        result=$(printf "$1" | sed -e 's/a//gI; s/e//gI; s/i//gI; s/o//gI; s/u//gI' )
+        result=$(printf "$1" | sed -e 's/[aeiou]//gI' )
 fi
 echo "$result"
 exit 0
